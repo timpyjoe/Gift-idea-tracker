7175f3ee26a3cd93c5a5cb4ef4d2a58e069c45e3
FranklinBrad <franklin1jbradley@gmail.com>
2023-12-08 10:09:34 -0600

    started homepage

--- client/App.jsx
+++ client/App.jsx
@@ -1,30 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppProvider from './utils/AppProvider';
 import { Header, ProtectedRoute } from './components';
-import { HomePage, AuthPage, Logout, PrivatePage } from './pages/'
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './pages/HomePage';
 
 export default function App(){
 
   return (
-    <AppProvider>
-      <BrowserRouter>
-        <Header />
-        <div className="container pt-5">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/auth" element={<AuthPage />} />
-
-            <Route path="/private" element={
-              <ProtectedRoute>
-                <PrivatePage />
-              </ProtectedRoute>
-            }/>
-
-            <Route path="/logout" element={<Logout />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </AppProvider>
+   <>
+   <HomePage/>
+   </>
   )
 }
