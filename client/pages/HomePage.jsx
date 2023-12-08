431b9df4cd8afa0e7dc7b7ddca944bedd3444b93
Holly T <144973465+thompsonholly@users.noreply.github.com>
2023-12-08 14:28:20 -0600

    router still works

--- client/pages/HomePage.jsx
+++ client/pages/HomePage.jsx
@@ -2,62 +2,50 @@
 
 
 
-import Header from "../components/Header"
+
 import Button from 'react-bootstrap/Button';
 
 
 
 
 export default function HomePage() {
-  // <Header />
+
   return (
 
 
 
-      <main
-        className="main"
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-          backgroundImage: "url('https://images.unsplash.com/photo-1549465220-1a8b9238cd48?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lmdCUyMGJveHxlbnwwfHwwfHx8MA%3D%3D')",
-          backgroundSize: 'cover',
-        }}
-      >
-        <div className='nav' style={{
-          borderRadius: "20px", position: 'absolute', top: 0, width: '100%', display: "flex", justifyContent: "center",
-          alignItems: "center", paddingTop: "50px"
-        }}>
+    <main
+      className="main"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundImage: "url('https://images.unsplash.com/photo-1549465220-1a8b9238cd48?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lmdCUyMGJveHxlbnwwfHwwfHx8MA%3D%3D')",
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className='nav' style={{
+        borderRadius: "20px", position: 'absolute', top: 0, width: '100%', display: "flex", justifyContent: "center",
+        alignItems: "center", paddingTop: "50px"
+      }}>
 
 
-      </div> 
-  
-    
-
+      </div>
 
 
 
 
 
 
-//   <div
 
-//           style={{
-//             display: 'flex',
-//             flexDirection: 'column',
-//             alignItems: 'center',
-//           }}
-//         >
-//           <h1
-//             className="header"
-//             style={{
-//               textAlign: 'center',
-//               color: 'white',
-//               fontSize: '100px',
-//               fontFamily: 'Cooper Std'
 
-//             }}
+
+
+
+
+
+
 
 
       <h1>gifty.
