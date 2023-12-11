dfb9d46fd0273b8b2a1993d388e5f54912873be7
Holly T <144973465+thompsonholly@users.noreply.github.com>
2023-12-11 11:29:18 -0600

    working in test folder to determine what logged in page for giftee looks like

--- client/pages/GifteePage.jsx
+++ client/pages/GifteePage.jsx
@@ -1,7 +1,12 @@
-import { useRouteError } from "react-router-dom";
-import { useEffect, useState } from "react";
+// import { useRouteError } from "react-router-dom";
+// import { useEffect, useState } from "react";
+
+// import Giftee from "../../server/models/Giftee";
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import React from 'react'
+import Stack from 'react-bootstrap/Stack';
 import "./style.css"
-import Giftee from "../../server/models/Giftee";
 
 function GifteePage() {
   return (
@@ -10,26 +15,63 @@ function GifteePage() {
 
     <main
       className="main-container"
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        border: '1px solid green',
-      }}
+    //   style={{
+    //     display: 'flex',
+    //     left- component
+    //     // justifyContent: 'flex-start',
+    //     // alignItems: 'center',
+    //     height: '100vh',
+    // border: '1px solid green',
+    //   }}
 
-    ><h2>List of Gifts</h2>
+    ><h2 className="about-likes-container">About and Likes</h2>
       <ul>
         <li></li>
-
         <li></li>
-
         <li></li>
-
-        <li></li>
-
       </ul>
-    </main>
+
+      <CardGroup className="gift-list-container">  <h2> List of Gifts</h2 >
+
+        <Stack gap={2} className="col-md-5 mx-auto">
+          <Card className="list-items">
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in
+                to additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Stack>
+
+        <Card className="list-items">
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{' '}
+            </Card.Text>
+          </Card.Body>
+
+        </Card>
+        <Card className="list-items">
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in
+              to additional content. This card has even longer content than the
+              first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+
+        </Card>
+      </CardGroup>
+
+    </main >
 
   )
 }
