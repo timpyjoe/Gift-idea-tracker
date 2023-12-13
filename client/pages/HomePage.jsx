ab4e3da2de01a3aaa088b6b76a4d61dae584660e
seanwsutter <seanwsutter@gmail.com>
2023-12-13 16:22:31 -0600

    nav css fixed

--- client/pages/HomePage.jsx
+++ client/pages/HomePage.jsx
@@ -17,7 +17,7 @@ export default function HomePage() {
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
-        backgroundImage: "url('https://images.unsplash.com/photo-1549465220-1a8b9238cd48?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lmdCUyMGJveHxlbnwwfHwwfHx8MA%3D%3D')",
+        backgroundImage: "url('https://i.imgur.com/3ocYD4J.png')",
         backgroundSize: 'cover',
       }}
     >
